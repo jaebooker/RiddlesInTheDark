3575039933ac6fbcbd8c60799e93fa05e90fa3a9
jaebooker <jaesonbooker@gmail.com>
2018-10-19 11:44:01 -0700

    first

---
+++ RiddlesInTheDark/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RiddlesInTheDark
+//
+//  Created by Jaeson Booker on 10/19/18.
+//  Copyright © 2018 Jaeson Booker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
