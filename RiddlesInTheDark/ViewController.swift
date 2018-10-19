bebf4d3c983c922de881edf8e7d67dd825bb5c12
jaebooker <jaesonbooker@gmail.com>
2018-10-19 12:35:17 -0700

    added date

--- RiddlesInTheDark/ViewController.swift
+++ RiddlesInTheDark/ViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let formatter : DateFormatter = DateFormatter()
     var todaysRiddle: String = "Why do you park in the driveway, but drive in a parkway?"
     var yesterdaysAnswer: String = "Riddle: If the plural for 'goose' is 'geese', why isn't 'moose' meese? Answer: Because the English languages is stupid and you should stop asking stupid questions, Timmy! Now shut up and eat your green vegetables!"
     var buttonSwitch = 0
@@ -23,6 +24,9 @@ class ViewController: UIViewController {
     }
     @IBOutlet weak var riddleQuestion: UITextView!
     override func viewDidLoad() {
+        formatter.dateFormat = "d/M/yy"
+        let myStr : String = formatter.string(from:   NSDate.init(timeIntervalSinceNow: 0) as Date)
+        print(myStr)
         riddleQuestion.text = todaysRiddle
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
